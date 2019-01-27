c95890d52083c36dda4b62107c4a4d95880c3894
Rumkox <camilo42.es@gmail.com>
2019-01-27 19:07:02 +0100

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'home/societes'
+  get 'societes', to: 'home#societes'
+  get 'solutions', to: 'home#solutions'
+  get 'application-mobile', to: 'home#application-mobile'
+  get 'marches', to: 'home#marches'
+  get 'equipes', to: 'home#equipes'
+  get 'contactez-nous', to: 'home#contactez-nous'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
