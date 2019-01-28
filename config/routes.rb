7ecff7f93d73f6dfcdd0e101d14dddb4c6af5968
freespirit <eric_ling@hotmail.fr>
2019-01-28 19:40:31 +0100

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
-Rails.application.routes.draw do
-  root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+    root to: 'home#index'
+    get 'societes', to: 'home#societes'
+    get 'solutions', to: 'home#solutions'
+    get 'application-mobile', to: 'home#application-mobile'
+    get 'marches', to: 'home#marches'
+    get 'equipes', to: 'home#equipes'
+    get 'contactez-nous', to: 'home#contactez-nous'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
